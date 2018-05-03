0061da0b74597e8193c154d2f5680ee59e610f50
hugo araujo de sousa <hugosousa@araguaia.grad.dcc.ufmg.br>
2018-05-03 18:52:15 -0300

    tp1: add point generator

--- tp1/source/PointGen.py
+++ tp1/source/PointGen.py
@@ -3,6 +3,18 @@
 '''
 Trabalho Pratico 1: Inteligência Artificial
 Autor: Hugo Araujo de Sousa (2013007463)
-PointGen.py: Iterative deepening depth-first search.
+PointGen.py: Point generator to run
 '''
 
+from random import *
+import sys
+
+seed()
+
+num_points = int(sys.argv[1])
+
+for i in range(num_points):
+	for j in range(4):
+		print(randint(0, 255), end=' ')
+
+	print()
