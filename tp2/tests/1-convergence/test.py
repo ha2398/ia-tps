b24dffea0124b1fe09e92c7732269e44f64c3749
Hugo Sousa <hsousa@gmx.com>
2018-07-05 15:20:41 -0300

    tp2: fix plot layout in convergence test

--- tp2/tests/1-convergence/test.py
+++ tp2/tests/1-convergence/test.py
@@ -2,22 +2,31 @@
 
 # Run convergence experiment.
 
+import argparse
 import matplotlib.pyplot as plt
 import os
 import shutil
 import subprocess as sp
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', dest='SEEDS', default=5, type=int,
+	help='Number of seeds.')
+parser.add_argument('-j', dest='JOBS', default=2, type=int,
+	help='Number of jobs.')
+parser.add_argument('-i', dest='ITER', default=100, type=int,
+	help='Number of iterations.')
+
+args = parser.parse_args()
 
 EXE = '../../source/tp2.py'
 MAPS  = '../../maps/'
 ALPHA = 0.3
 GAMMA = 0.9
-ITER = 1000
 EPSILON = 1
-SEEDS = 5
-JOBS = 4
 
 plt.xlabel('Tempo (iterações)')
-plt.ylabel(r'$\sum_{i,j} max(Q_{i,j}$')
+plt.ylabel(r'$\sum_{i,j} max(Q_{i,j})$')
 plt.title('Convergência da política ótima')
 plt.grid(True)
 
@@ -40,17 +49,18 @@ for m in maps:
 	map_dir = m.split('.')[0]
 	os.mkdir(map_dir)
 
-	for s in range(SEEDS):
+	for s in range(args.SEEDS):
 		csv_name = map_dir + '/qsum_s' + str(s) + '.csv' 
 		cmds.write('{} {} {} {} {} -s {} -e {} -q {}\n'.format(
-			EXE, MAPS+m, ALPHA, GAMMA, ITER, s, EPSILON, csv_name))
+			EXE, MAPS+m, ALPHA, GAMMA, args.ITER, s, EPSILON, csv_name))
 
 cmds.close()
 
 # Run commands
 print('RUNNING COMMANDS\n')
+
 cmds = open('cmds.txt', 'r')
-sp.call(['parallel', '-j', str(JOBS), '--bar'], stdin=cmds)
+sp.call(['parallel', '-j', str(args.JOBS), '--bar'], stdin=cmds)
 cmds.close()
 print()
 
@@ -58,7 +68,7 @@ data = {}
 # Get data and plot
 print('PLOTTING DATA\n')
 for m in maps:
-	data[m] = [0.] * ITER
+	data[m] = [0.] * args.ITER
 	map_dir = m.split('.')[0]
 	csv_files = os.listdir(map_dir)
 
@@ -80,12 +90,13 @@ for m in maps:
 		csv_f.close()
 
 # Plot
-x_axis = list(range(1, ITER+1))
+x_axis = list(range(1, args.ITER+1))
 legend = []
 
 for m in data:
 	legend.append(m)
-	plt.plot(x_axis, list(map(lambda x: x/SEEDS, data[m])))
+	plt.plot(x_axis, list(map(lambda x: x/args.SEEDS, data[m])))
 
 plt.legend(legend, loc='upper left')
-plt.show()
+plt.tight_layout()
+plt.savefig('convergence.png')
